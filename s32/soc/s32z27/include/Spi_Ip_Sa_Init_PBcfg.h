841320628c1caf708811e9c32f658cf8320f0019
Dat Nguyen Duy <dat.nguyenduy@nxp.com>
2022-10-22 07:20:39 +0700

    s32: s32z27: add configuration for SPI
    Add configuration for SPI that was generated by using
    NXP S32 Design Studio for S32 platform
    
    Signed-off-by: Dat Nguyen Duy <dat.nguyenduy@nxp.com>

---
+++ s32/soc/s32z27/include/Spi_Ip_Sa_Init_PBcfg.h
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2022 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SPI_IP_SA_PBCFG_Init_H
+#define SPI_IP_SA_PBCFG_Init_H
+
+/**   
+*   @file    Spi_Ip_PBcfg.h
+*   @version 0.8.0
+*
+*   @brief   AUTOSAR Spi - Post-Build(PB) configuration file code template.
+*   @details Code template for Post-Build(PB) configuration file generation.
+*
+*   @addtogroup SPI_IP_DRIVER_CONFIGURATION Spi Ip Driver Configuration
+*   @{
+*/
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*==================================================================================================
+                                         INCLUDE FILES
+ 1) system and project includes
+ 2) needed interfaces from external units
+ 3) internal and external interfaces from this unit
+==================================================================================================*/
+
+/*==================================================================================================
+*                              SOURCE FILE VERSION INFORMATION
+==================================================================================================*/
+
+#define SPI_IP_SA_INIT_MODULE_ID_PBCFG                        83
+#define SPI_IP_SA_INIT_VENDOR_ID_PBCFG                        43
+#define SPI_IP_SA_INIT_AR_RELEASE_MAJOR_VERSION_PBCFG         4
+#define SPI_IP_SA_INIT_AR_RELEASE_MINOR_VERSION_PBCFG         4
+#define SPI_IP_SA_INIT_AR_RELEASE_REVISION_VERSION_PBCFG      0
+#define SPI_IP_SA_INIT_SW_MAJOR_VERSION_PBCFG                 0
+#define SPI_IP_SA_INIT_SW_MINOR_VERSION_PBCFG                 8
+#define SPI_IP_SA_INIT_SW_PATCH_VERSION_PBCFG                 0
+
+/*==================================================================================================
+                                      FILE VERSION CHECKS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                          CONSTANTS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                      DEFINES AND MACROS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                             ENUMS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                STRUCTURES AND OTHER TYPEDEFS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                GLOBAL VARIABLE DECLARATIONS
+==================================================================================================*/
+#define SPI_IP_SA_CONFIG_Init_PB \
+    extern const Spi_Ip_ExternalDeviceType Spi_Ip_DeviceAttributes_SpiExternalDevice_0_Spi_0_Instance_0_Init; \
+    extern const Spi_Ip_ExternalDeviceType Spi_Ip_DeviceAttributes_SpiExternalDevice_1_Spi_0_Instance_0_Init; \
+    extern const Spi_Ip_ExternalDeviceType Spi_Ip_DeviceAttributes_SpiExternalDevice_2_Spi_0_Instance_0_Init; \
+    extern const Spi_Ip_ExternalDeviceType Spi_Ip_DeviceAttributes_SpiExternalDevice_3_Spi_0_Instance_0_Init; \
+    extern const Spi_Ip_ExternalDeviceType Spi_Ip_DeviceAttributes_SpiExternalDevice_4_Spi_0_Instance_0_Init; \
+    extern const Spi_Ip_ExternalDeviceType Spi_Ip_DeviceAttributes_SpiExternalDevice_0_Spi_4_Instance_4_Init; \
+    extern const Spi_Ip_ExternalDeviceType Spi_Ip_DeviceAttributes_SpiExternalDevice_1_Spi_4_Instance_4_Init; \
+    extern const Spi_Ip_ExternalDeviceType Spi_Ip_DeviceAttributes_SpiExternalDevice_2_Spi_4_Instance_4_Init; \
+    extern const Spi_Ip_ExternalDeviceType Spi_Ip_DeviceAttributes_SpiExternalDevice_3_Spi_4_Instance_4_Init; \
+    extern const Spi_Ip_ExternalDeviceType Spi_Ip_DeviceAttributes_SpiExternalDevice_4_Spi_4_Instance_4_Init; \
+    extern const Spi_Ip_ConfigType Spi_Ip_PhyUnitConfig_SpiPhyUnit_0_Instance_0_Init; \
+    extern const Spi_Ip_ConfigType Spi_Ip_PhyUnitConfig_SpiPhyUnit_1_Instance_1_Init; \
+    extern const Spi_Ip_ConfigType Spi_Ip_PhyUnitConfig_SpiPhyUnit_2_Instance_2_Init; \
+    extern const Spi_Ip_ConfigType Spi_Ip_PhyUnitConfig_SpiPhyUnit_3_Instance_3_Init; \
+    extern const Spi_Ip_ConfigType Spi_Ip_PhyUnitConfig_SpiPhyUnit_4_Instance_4_Init; \
+    extern const Spi_Ip_ConfigType Spi_Ip_PhyUnitConfig_SpiPhyUnit_5_Instance_5_Init; \
+    extern const Spi_Ip_ConfigType Spi_Ip_PhyUnitConfig_SpiPhyUnit_6_Instance_6_Init; \
+    extern const Spi_Ip_ConfigType Spi_Ip_PhyUnitConfig_SpiPhyUnit_7_Instance_7_Init; \
+    extern const Spi_Ip_ConfigType Spi_Ip_PhyUnitConfig_SpiPhyUnit_8_Instance_8_Init; \
+    extern const Spi_Ip_ConfigType Spi_Ip_PhyUnitConfig_SpiPhyUnit_9_Instance_9_Init; \
+
+/*==================================================================================================
+                                   LOCAL FUNCTION PROTOTYPES
+==================================================================================================*/
+
+
+/*==================================================================================================
+                                       LOCAL FUNCTIONS
+==================================================================================================*/
+
+
+/*==================================================================================================
+                                       GLOBAL FUNCTIONS
+==================================================================================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
+
+/** @} */
+
+
