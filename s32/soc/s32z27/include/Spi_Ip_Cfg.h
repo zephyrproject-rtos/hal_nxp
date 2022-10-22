87cb0d38913e88301a057c6660b0ccd49013c327
Dat Nguyen Duy <dat.nguyenduy@nxp.com>
2022-10-22 07:20:39 +0700

    s32: s32z27: add configuration for SPI
    Add configuration for SPI that was generated by using
    NXP S32 Design Studio for S32 platform
    
    Signed-off-by: Dat Nguyen Duy <dat.nguyenduy@nxp.com>

---
+++ s32/soc/s32z27/include/Spi_Ip_Cfg.h
@@ -0,0 +1,207 @@
+/*
+ * Copyright 2022 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SPI_IP_CFG_H
+#define SPI_IP_CFG_H
+
+/**
+*   @file    Spi_Ip_Cfg.h
+*   @version 0.8.0
+*
+*   @brief   AUTOSAR Spi - Spi configuration header file.
+*   @details This file is the header containing all the necessary information for SPI
+*            module configuration(s).
+*   @addtogroup SPI_IP_DRIVER_CONFIGURATION Spi Ip Driver Configuration
+*   @{
+*/
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+/*==================================================================================================
+*                                        INCLUDE FILES
+* 1) system and project includes
+* 2) needed interfaces from external units
+* 3) internal and external interfaces from this unit
+* 4) user callback header files
+==================================================================================================*/
+#include "Mcal.h"
+#include "OsIf.h"
+#include "Spi_Ip_Sa_Init_PBcfg.h"
+#include "S32Z27_SPI.h"
+/*==================================================================================================
+*                              SOURCE FILE VERSION INFORMATION
+==================================================================================================*/
+#define SPI_IP_VENDOR_ID_CFG                       43
+#define SPI_IP_AR_RELEASE_MAJOR_VERSION_CFG        4
+#define SPI_IP_AR_RELEASE_MINOR_VERSION_CFG        4
+#define SPI_IP_AR_RELEASE_REVISION_VERSION_CFG     0
+#define SPI_IP_SW_MAJOR_VERSION_CFG                0
+#define SPI_IP_SW_MINOR_VERSION_CFG                8
+#define SPI_IP_SW_PATCH_VERSION_CFG                0
+
+/*==================================================================================================
+                                      FILE VERSION CHECKS
+==================================================================================================*/
+#ifndef DISABLE_MCAL_INTERMODULE_ASR_CHECK
+    /* Check if current file and Mcal header file are of the same Autosar version */
+    #if ((SPI_IP_AR_RELEASE_MAJOR_VERSION_CFG != MCAL_AR_RELEASE_MAJOR_VERSION) || \
+         (SPI_IP_AR_RELEASE_MINOR_VERSION_CFG != MCAL_AR_RELEASE_MINOR_VERSION))
+    #error "AutoSar Version Numbers of Spi_Ip_Cfg.h and Mcal.h are different"
+    #endif
+
+    /* Check if the current file and OsIf.h header file are of the same version */
+    #if ((SPI_IP_AR_RELEASE_MAJOR_VERSION_CFG != OSIF_AR_RELEASE_MAJOR_VERSION) || \
+         (SPI_IP_AR_RELEASE_MINOR_VERSION_CFG != OSIF_AR_RELEASE_MINOR_VERSION) \
+        )
+        #error "AutoSar Version Numbers of Spi_Ip_Cfg.h and OsIf.h are different"
+    #endif
+#endif
+
+/* Check if Spi_Ip_INIT_PBcfg header file and Spi configuration header file are of the same vendor */
+#if (SPI_IP_SA_INIT_VENDOR_ID_PBCFG != SPI_IP_VENDOR_ID_CFG)
+    #error "Spi_Ip_Sa_INIT_PBcfg.h and Spi_Ip_Cfg.h have different vendor IDs"
+#endif
+    /* Check if Spi_Ip_INIT_PBcfg header file and Spi  configuration header file are of the same Autosar version */
+#if ((SPI_IP_SA_INIT_AR_RELEASE_MAJOR_VERSION_PBCFG != SPI_IP_AR_RELEASE_MAJOR_VERSION_CFG) || \
+     (SPI_IP_SA_INIT_AR_RELEASE_MINOR_VERSION_PBCFG != SPI_IP_AR_RELEASE_MINOR_VERSION_CFG) || \
+     (SPI_IP_SA_INIT_AR_RELEASE_REVISION_VERSION_PBCFG != SPI_IP_AR_RELEASE_REVISION_VERSION_CFG))
+#error "AutoSar Version Numbers of Spi_Ip_Sa_INIT_PBcfg.h and Spi_Ip_Cfg.h are different"
+#endif
+/* Check if Spi_Ip_INIT_PBcfg header file and Spi configuration header file are of the same software version */
+#if ((SPI_IP_SA_INIT_SW_MAJOR_VERSION_PBCFG != SPI_IP_SW_MAJOR_VERSION_CFG) || \
+     (SPI_IP_SA_INIT_SW_MINOR_VERSION_PBCFG != SPI_IP_SW_MINOR_VERSION_CFG) || \
+     (SPI_IP_SA_INIT_SW_PATCH_VERSION_PBCFG != SPI_IP_SW_PATCH_VERSION_CFG))
+#error "Software Version Numbers of Spi_Ip_Sa_INIT_PBcfg.h and Spi_Ip_Cfg.h are different"
+#endif
+/*==================================================================================================
+                                            CONSTANTS
+==================================================================================================*/
+#define SPI_IP_CONFIG_EXT \
+    SPI_IP_SA_CONFIG_Init_PB \
+
+/** @brief Total number of SpiPhyUnit configured as DSPI. */
+#define SPI_IP_NUMBER_OF_INSTANCES 10U
+
+/** @brief Number of FIFO entries supported */
+#define SPI_IP_FIFO_SIZE_U16    ((uint16)5U)
+
+/** @brief HWUnits enabled/disabled */
+#define SPI_IP_0_ENABLED  (STD_ON)
+#define SPI_IP_1_ENABLED  (STD_ON)
+#define SPI_IP_2_ENABLED  (STD_ON)
+#define SPI_IP_3_ENABLED  (STD_ON)
+#define SPI_IP_4_ENABLED  (STD_ON)
+#define SPI_IP_5_ENABLED  (STD_ON)
+#define SPI_IP_6_ENABLED  (STD_ON)
+#define SPI_IP_7_ENABLED  (STD_ON)
+#define SPI_IP_8_ENABLED  (STD_ON)
+#define SPI_IP_9_ENABLED  (STD_ON)
+#define SPI_IP_SLAVE_SUPPORT  (STD_OFF)
+
+/**
+* @brief Defines if transfers are made using DMA or FIFO.
+* @details Defines if transfers are made using DMA or FIFO.
+*/
+#define SPI_IP_DMA_USED    (STD_OFF)
+
+/**
+* @brief If enabled, allows dual MCU clock configuration settings.
+* @details If enabled, allows dual MCU clock configuration settings.
+*/
+#define SPI_IP_DUAL_CLOCK_MODE  (STD_OFF)
+
+/**
+* @brief          Enable Dma Fast transfer support.
+* @details        When SpiEnableDmaFastTransferSupport = TRUE,
+*                 the SPI driver can be supported to transmit multiple transfers and CPU used only for processing end of sequence transfer.
+*/
+#define SPI_IP_ENABLE_DMAFASTTRANSFER_SUPPORT  (STD_OFF)
+
+ /**
+* @brief          Enable User Mode Support.
+* @details        When SpiEnableUserModeSupport = TRUE,
+*                 the SPI driver can be executed from both supervisor and user mode.
+*
+* @api
+*/
+#define SPI_IP_ENABLE_USER_MODE_SUPPORT           STD_OFF
+
+#ifndef MCAL_ENABLE_USER_MODE_SUPPORT
+ #ifdef SPI_IP_ENABLE_USER_MODE_SUPPORT
+  #if (STD_ON == SPI_IP_ENABLE_USER_MODE_SUPPORT)
+   #error MCAL_ENABLE_USER_MODE_SUPPORT is not enabled. For running  Spi in user mode the MCAL_ENABLE_USER_MODE_SUPPORT needs to be defined
+  #endif /* (STD_ON == SPI_IP_ENABLE_USER_MODE_SUPPORT) */
+ #endif /* ifdef MCAL_ENABLE_USER_MODE_SUPPORT */
+#endif /* ifndef MCAL_ENABLE_USER_MODE_SUPPORT*/
+
+/**
+* @brief SPI registers is controlled by REG_PROT IP.
+*/
+#define SPI_IP_REG_PROT_AVAILABLE   (STD_ON)
+
+#if (STD_ON == SPI_IP_REG_PROT_AVAILABLE)
+/**
+* @brief The protection size
+*/
+    #define SPI_IP_PROT_MEM_U32                           ((uint32)4U)
+#endif
+/**
+* @brief Maximum of frame size supported.
+*/
+#define SPI_IP_FRAMESIZE_MAX_U8                              ((uint8)32U)
+
+/**
+* @brief Minimum of frame size supported.
+*/
+#define SPI_IP_FRAMESIZE_MIN_U8                              ((uint8)4U)
+
+/**
+* @brief Define Timeout Method.
+* @details Define a certain timeout method from OsIf will be used in the driver.
+*/
+#define SPI_IP_TIMEOUT_METHOD     (OSIF_COUNTER_SYSTEM)
+
+/**
+* @brief Switches ON or OFF for the detection and reporting of development errors(API parameter checking) at IP level.
+*/
+/** @implements SPI_IP_DEV_ERROR_DETECT_define  */
+#define SPI_IP_DEV_ERROR_DETECT     (STD_ON)
+/*==================================================================================================
+ *                                     DEFINES AND MACROS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                            ENUMS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                               STRUCTURES AND OTHER TYPEDEFS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                GLOBAL VARIABLE DECLARATIONS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                    FUNCTION PROTOTYPES
+==================================================================================================*/
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
+/** @} */
+
+
