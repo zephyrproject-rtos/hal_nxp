0d9de54f25f897bd8b50019921f9bb5912989ded
Manuel Arguelles <manuel.arguelles@nxp.com>
2022-11-09 15:08:46 +0700

    s32: s32z27: update SPI config to RTD 0.9.0
    Update tool-generated code for SPI configuration to Real-Time Drivers
    0.9.0 for S32Z/E.
    
    Signed-off-by: Manuel Arguelles <manuel.arguelles@nxp.com>

--- s32/soc/s32z27/include/Spi_Ip_Sa_Init_PBcfg.h
+++ s32/soc/s32z27/include/Spi_Ip_Sa_Init_PBcfg.h
@@ -4,19 +4,19 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#ifndef SPI_IP_SA_PBCFG_Init_H
-#define SPI_IP_SA_PBCFG_Init_H
+#ifndef SPI_IP_SA_PBCFG_INIT_H
+#define SPI_IP_SA_PBCFG_INIT_H
 
 #define SPI_IP_SA_INIT_MODULE_ID_PBCFG                        83
 #define SPI_IP_SA_INIT_VENDOR_ID_PBCFG                        43
 #define SPI_IP_SA_INIT_AR_RELEASE_MAJOR_VERSION_PBCFG         4
-#define SPI_IP_SA_INIT_AR_RELEASE_MINOR_VERSION_PBCFG         4
+#define SPI_IP_SA_INIT_AR_RELEASE_MINOR_VERSION_PBCFG         7
 #define SPI_IP_SA_INIT_AR_RELEASE_REVISION_VERSION_PBCFG      0
 #define SPI_IP_SA_INIT_SW_MAJOR_VERSION_PBCFG                 0
-#define SPI_IP_SA_INIT_SW_MINOR_VERSION_PBCFG                 8
+#define SPI_IP_SA_INIT_SW_MINOR_VERSION_PBCFG                 9
 #define SPI_IP_SA_INIT_SW_PATCH_VERSION_PBCFG                 0
 
 /* Not used, but required for build */
-#define SPI_IP_SA_CONFIG_Init_PB
+#define SPI_IP_SA_CONFIG_INIT_PB
 
 #endif
