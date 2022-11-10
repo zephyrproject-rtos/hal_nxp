f9555cfd1120fbda1e35f8da8fffcb96b92f7899
Cong Nguyen Huu <cong.nguyenhuu@nxp.com>
2022-11-10 21:48:08 +0700

    s32: s32z27: clean-up configuration for CANEXCEL
    This auto-generated file is not used by Zephyr integration, so a minimal
    manually modified version is kept in order to build. CANEXCEL configuration
    is generated in the Zephyr shim driver.
    
    Signed-off-by: Cong Nguyen Huu <cong.nguyenhuu@nxp.com>

--- s32/soc/s32z27/include/CanEXCEL_Ip_Sa_Init_PBcfg.h
+++ s32/soc/s32z27/include/CanEXCEL_Ip_Sa_Init_PBcfg.h
@@ -4,30 +4,16 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+/*
+ * This auto-generated file is not used by Zephyr integration, so a minimal
+ * manually modified version is kept in order to build. CanEXCEL default
+ * configuration is generated in the Zephyr shim driver, see
+ * zephyr/drivers/can/can_nxp_s32_canxl.c
+ */
+
 #ifndef CANEXCEL_IP_SA_INIT_PBCFG_H
 #define CANEXCEL_IP_SA_INIT_PBCFG_H
-/**
-*   @file CanEXCEL_IP_PBcfg.h
-*
-*   @addtogroup CanEXCEL
-*   @{
-*/
-#ifdef __cplusplus
-extern "C"{
-#endif
-/*==================================================================================================
-*                                        INCLUDE FILES
-* 1) system and project includes
-* 2) needed interfaces from external units
-* 3) internal and external interfaces from this unit
-==================================================================================================*/
 
-/*==================================================================================================
-*                              SOURCE FILE VERSION INFORMATION
-==================================================================================================*/
-/*
-* @file           CanEXCEL_Ip_Sa_Init_PBcfg.h
-*/
 #define CANEXCEL_IP_SA_VENDOR_ID_INIT_PBCFG_H                     43
 #define CANEXCEL_IP_SA_AR_RELEASE_MAJOR_VERSION_INIT_PBCFG_H      4
 #define CANEXCEL_IP_SA_AR_RELEASE_MINOR_VERSION_INIT_PBCFG_H      7
@@ -35,20 +21,8 @@ extern "C"{
 #define CANEXCEL_IP_SA_SW_MAJOR_VERSION_INIT_PBCFG_H              0
 #define CANEXCEL_IP_SA_SW_MINOR_VERSION_INIT_PBCFG_H              9
 #define CANEXCEL_IP_SA_SW_PATCH_VERSION_INIT_PBCFG_H              0
-/*==================================================================================================
-*                                      DEFINES AND MACROS
-==================================================================================================*/
-#define CANEXCEL_IP_INIT_SA_PB_CFG \
-extern const Canexcel_Ip_ConfigType CanXL_Config0; \
-extern const Canexcel_Ip_ConfigType CanXL_Config1; 
-#define INST_INIT_CANXL_0 0U
-#define INST_INIT_CANXL_1 1U
-#define CANEXCEL_IP_INIT_SA_STATE_PB_CFG \
-extern Canexcel_Ip_StateType CanEXCEL_Init_State0; \
-extern Canexcel_Ip_StateType CanEXCEL_Init_State1; 
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
 
-#endif /* CANEXCEL_IP_PBCFG_H */
+#define CANEXCEL_IP_INIT_SA_PB_CFG
+#define CANEXCEL_IP_INIT_SA_STATE_PB_CFG
 
+#endif /* CANEXCEL_IP_SA_INIT_PBCFG_H */
