8e27a6e7fc261743e08fbf19c9162ce7c25af3d9
Manuel Argüelles <manuel.arguelles@nxp.com>
2023-05-26 00:00:00 +0000

    s32: soc: s32k344: patch ICU generated code for compatibility
    Remove U suffix from SIUL2_ICU_IP_NUM_OF_CHANNELS so that can be used
    with listify in the shim driver. Clean up auto-generated code that is
    not used by this Zephyr integration.
    
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

--- s32/soc/s32k344/include/Siul2_Icu_Ip_SA_Init_PBcfg.h
+++ s32/soc/s32k344/include/Siul2_Icu_Ip_SA_Init_PBcfg.h
@@ -4,34 +4,15 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+/*
+ * This auto-generated file is not used by Zephyr integration, so a minimal
+ * manually modified version is kept in order to build. SIUL2 ICU configuration
+ * is generated in the Zephyr shim driver, see zephyr/drivers/interrupt-controller/intc_eirq_s32.c
+ */
+
 #ifndef SIUL2_ICU_IP_SA_INIT_PBCFG_H
 #define SIUL2_ICU_IP_SA_INIT_PBCFG_H
 
-/**
- *   @file    Siul2_Icu_Ip_SA_Init_PBCfg.h
- *   @version 3.0.0
- *
- *   @brief   AUTOSAR Icu - contains the data exported by the Icu module
- *   @details Contains the information that will be exported by the module, as requested by Autosar.
- *
- *   @addtogroup siul2_icu_ip SIUL2 IPL
- *   @{
- */
-
-#ifdef __cplusplus
-extern "C"{
-#endif
-
- /*==================================================================================================
- *                                         INCLUDE FILES
- * 1) system and project includes
- * 2) needed interfaces from external units
- * 3) internal and external interfaces from this unit
- *================================================================================================*/
-#include "Siul2_Icu_Ip_Types.h"
-/*==================================================================================================
- *                              SOURCE FILE VERSION INFORMATION
- *================================================================================================*/
 #define SIUL2_ICU_IP_SA_INIT_PBCFG_VENDOR_ID                     43
 #define SIUL2_ICU_IP_SA_INIT_PBCFG_AR_RELEASE_MAJOR_VERSION      4
 #define SIUL2_ICU_IP_SA_INIT_PBCFG_AR_RELEASE_MINOR_VERSION      7
@@ -40,39 +21,7 @@ extern "C"{
 #define SIUL2_ICU_IP_SA_INIT_PBCFG_SW_MINOR_VERSION              0
 #define SIUL2_ICU_IP_SA_INIT_PBCFG_SW_PATCH_VERSION              0
 
-/*==================================================================================================
- *                                      FILE VERSION CHECKS
- *================================================================================================*/
-/* Check if source file and ICU header file are of the same vendor */
-#if (SIUL2_ICU_IP_SA_INIT_PBCFG_VENDOR_ID != SIUL2_ICU_IP_TYPES_VENDOR_ID)
-    #error "Siul2_Icu_Ip_SA_Init_PBcfg.h and Siul2_Icu_Ip_Types.h have different vendor IDs"
-#endif
-/* Check if source file and ICU header file are of the same AutoSar version */
-#if ((SIUL2_ICU_IP_SA_INIT_PBCFG_AR_RELEASE_MAJOR_VERSION    != SIUL2_ICU_IP_TYPES_AR_RELEASE_MAJOR_VERSION) || \
-     (SIUL2_ICU_IP_SA_INIT_PBCFG_AR_RELEASE_MINOR_VERSION    != SIUL2_ICU_IP_TYPES_AR_RELEASE_MINOR_VERSION) || \
-     (SIUL2_ICU_IP_SA_INIT_PBCFG_AR_RELEASE_REVISION_VERSION != SIUL2_ICU_IP_TYPES_AR_RELEASE_REVISION_VERSION))
-    #error "AutoSar Version Numbers of Siul2_Icu_Ip_SA_Init_PBcfg.h and Siul2_Icu_Ip_Types.h are different"
-#endif
-/* Check if source file and ICU header file are of the same Software version */
-#if ((SIUL2_ICU_IP_SA_INIT_PBCFG_SW_MAJOR_VERSION != SIUL2_ICU_IP_TYPES_SW_MAJOR_VERSION) || \
-     (SIUL2_ICU_IP_SA_INIT_PBCFG_SW_MINOR_VERSION != SIUL2_ICU_IP_TYPES_SW_MINOR_VERSION) || \
-     (SIUL2_ICU_IP_SA_INIT_PBCFG_SW_PATCH_VERSION != SIUL2_ICU_IP_TYPES_SW_PATCH_VERSION))
-    #error "Software Version Numbers of Siul2_Icu_Ip_SA_Init_PBcfg.h and Siul2_Icu_Ip_Types.h are different"
-#endif
-/*==================================================================================================
- *                                       GLOBAL CONSTANTS
- *================================================================================================*/
-#if (STD_ON == SIUL2_ICU_IP_USED)
-#define SIUL2_ICU_CONFIG_SA_INIT_PB  \
-    extern const Siul2_Icu_Ip_ChannelConfigType Siul2_Icu_Ip_0_ChannelConfig_PB_Init[32U]; \
-    extern const Siul2_Icu_Ip_InstanceConfigType Siul2_Icu_Ip_0_InstanceConfig_PB_Init; \
-    extern const Siul2_Icu_Ip_ConfigType Siul2_Icu_Ip_0_Config_PB_Init; \
-
-#endif /* SIUL2_ICU_IP_USED */
-#ifdef __cplusplus
-}
-#endif
-
-/** @} */
+/* Not used, but required to build */
+#define SIUL2_ICU_CONFIG_SA_INIT_PB
 
 #endif /* SIUL2_ICU_IP_PBCFG_SA_INIT_H */
