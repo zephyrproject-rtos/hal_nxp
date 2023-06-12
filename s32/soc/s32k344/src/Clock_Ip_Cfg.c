5b61e54b3261bf98e2cfec7949ca73e91ceba49b
Cong Nguyen Huu <cong.nguyenhuu@nxp.com>
2023-06-12 13:53:39 +0700

    s32: soc: s32k344: config CAN and CAN_PE CLK to 80Mhz
    config clock divide value to 1.
    
    Signed-off-by: Cong Nguyen Huu <cong.nguyenhuu@nxp.com>

--- s32/soc/s32k344/src/Clock_Ip_Cfg.c
+++ s32/soc/s32k344/src/Clock_Ip_Cfg.c
@@ -501,7 +501,7 @@ static const Clock_Ip_DividerConfigType Clock_Ip_DividerConfigurations_0[CLOCK_I
     #if CLOCK_IP_CONFIGURED_DIVIDERS_0_NO > 15U
     {
         FLEXCANA_CLK,                    /* name */
-        2U,                              /* value */
+        1U,                              /* value */
         {
             0U,
         }
@@ -511,7 +511,7 @@ static const Clock_Ip_DividerConfigType Clock_Ip_DividerConfigurations_0[CLOCK_I
     #if CLOCK_IP_CONFIGURED_DIVIDERS_0_NO > 16U
     {
         FLEXCANB_CLK,                    /* name */
-        2U,                              /* value */
+        1U,                              /* value */
         {
             0U,
         }
