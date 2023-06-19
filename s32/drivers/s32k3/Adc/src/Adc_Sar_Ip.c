89fdd682e34b2af800a81b868db5bb67c2485cd5
Cong Nguyen Huu <cong.nguyenhuu@nxp.com>
2023-06-19 17:29:52 +0700

    s32: drivers: s32k3: adc: patch to fix the issue clear the EOC flag
    Clear the EOC flag before Adc_Sar_CheckAndCallEocNotification() is
    called to avoid the race condition that prevents the next end of
    conversion callback to be executed, when a conversion is started
    inside the previous end of conversion completed callback.
    
    Signed-off-by: Cong Nguyen Huu <cong.nguyenhuu@nxp.com>

--- s32/drivers/s32k3/Adc/src/Adc_Sar_Ip.c
+++ s32/drivers/s32k3/Adc/src/Adc_Sar_Ip.c
@@ -1609,6 +1609,12 @@ static inline void Adc_CheckAndCallAllChannelNotification(const uint32 Instance)
     EocFlag  = (*IMRAddr);
     EocFlag &= (*ISRAddr);
     EocFlag &= (ADC_ISR_EOC_MASK | ADC_ISR_JEOC_MASK);
+
+    /* Clear EOC Flag */
+    if (EocFlag != 0)
+    {
+        *ISRAddr = EocFlag;
+    }
 #endif /* (STD_ON == ADC_SAR_IP_EOC_ENABLED) */
 
     for (VectAdr = 0U; VectAdr < Adc_Sar_Ip_au8AdcGroupCount[Instance]; VectAdr++)
@@ -1629,13 +1635,6 @@ static inline void Adc_CheckAndCallAllChannelNotification(const uint32 Instance)
         }
     }
 
-    /* CPR_RTD_00664 */
-#if (STD_ON == ADC_SAR_IP_EOC_ENABLED)
-    if (TRUE == CeocfrFlag)
-    {
-        *ISRAddr = EocFlag;
-    }
-#endif /* (STD_ON == ADC_SAR_IP_EOC_ENABLED) */
 #if (STD_ON == ADC_SAR_IP_WDG_ENABLED)
     if (WtisrMask != 0U)
     {
