8432b8e824fd6292d0fcf7774127efe14ce126f2
Dat Nguyen Duy <dat.nguyenduy@nxp.com>
2023-06-26 15:35:15 +0700

    s32: soc: s32k344: add eMIOS Mcl, eMIOS Pwm specific code
    Code is auto generated by S32 Design Studio for S32 platform
    
    Signed-off-by: Dat Nguyen Duy <dat.nguyenduy@nxp.com>

---
+++ s32/soc/s32k344/include/Emios_Mcl_Ip_Cfg_Defines.h
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2023 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef EMIOS_MCL_IP_CFG_DEFINES_H
+#define EMIOS_MCL_IP_CFG_DEFINES_H
+/**
+*   @file Emios_Mcl_Ip_Cfg_Defines.h
+*
+*   @version 3.0.0
+*
+*   @brief   AUTOSAR Mcl - MCL driver header file.
+*   @details 
+*
+*   @addtogroup MCL_DRIVER MCL Driver
+*   @{
+*/
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*==================================================================================================
+*                                        INCLUDE FILES
+* 1) system and project includes
+* 2) needed interfaces from external units
+* 3) internal and external interfaces from this unit
+==================================================================================================*/
+#include "Emios_Mcl_Ip_Cfg_DeviceRegisters.h"
+#include "Emios_Pwm_Ip_CfgDefines.h"
+
+/*==================================================================================================
+*                               HEADER FILE VERSION INFORMATION
+==================================================================================================*/
+#define EMIOS_MCL_IP_CFG_DEFINES_VENDOR_ID                       43
+#define EMIOS_MCL_IP_CFG_DEFINES_AR_RELEASE_MAJOR_VERSION        4
+#define EMIOS_MCL_IP_CFG_DEFINES_AR_RELEASE_MINOR_VERSION        7
+#define EMIOS_MCL_IP_CFG_DEFINES_AR_RELEASE_REVISION_VERSION     0
+#define EMIOS_MCL_IP_CFG_DEFINES_SW_MAJOR_VERSION                3
+#define EMIOS_MCL_IP_CFG_DEFINES_SW_MINOR_VERSION                0
+#define EMIOS_MCL_IP_CFG_DEFINES_SW_PATCH_VERSION                0
+/*==================================================================================================
+*                                      FILE VERSION CHECKS
+==================================================================================================*/
+#if (EMIOS_MCL_IP_CFG_DEFINES_VENDOR_ID != EMIOS_MCL_IP_CFG_DEVICEREGISTERS_VENDOR_ID)
+    #error "Emios_Mcl_Ip_Cfg_Defines.h and Emios_Mcl_Ip_Cfg_DeviceRegisters.h have different vendor ids"
+#endif
+
+/* Check if  header file and Emios_Mcl_Ip_Cfg_DeviceRegisters.h file are of the same Autosar version */
+#if ((EMIOS_MCL_IP_CFG_DEFINES_AR_RELEASE_MAJOR_VERSION != EMIOS_MCL_IP_CFG_DEVICEREGISTERS_AR_RELEASE_MAJOR_VERSION) || \
+     (EMIOS_MCL_IP_CFG_DEFINES_AR_RELEASE_MINOR_VERSION != EMIOS_MCL_IP_CFG_DEVICEREGISTERS_AR_RELEASE_MINOR_VERSION) || \
+     (EMIOS_MCL_IP_CFG_DEFINES_AR_RELEASE_REVISION_VERSION != EMIOS_MCL_IP_CFG_DEVICEREGISTERS_AR_RELEASE_REVISION_VERSION))
+    #error "AutoSar Version Numbers of Emios_Mcl_Ip_Cfg_Defines.h and Emios_Mcl_Ip_Cfg_DeviceRegisters.h are different"
+#endif
+
+/* Check if header file and Emios_Mcl_Ip_Cfg_DeviceRegisters.h file are of the same Software version */
+#if ((EMIOS_MCL_IP_CFG_DEFINES_SW_MAJOR_VERSION != EMIOS_MCL_IP_CFG_DEVICEREGISTERS_SW_MAJOR_VERSION) || \
+     (EMIOS_MCL_IP_CFG_DEFINES_SW_MINOR_VERSION != EMIOS_MCL_IP_CFG_DEVICEREGISTERS_SW_MINOR_VERSION) || \
+     (EMIOS_MCL_IP_CFG_DEFINES_SW_PATCH_VERSION != EMIOS_MCL_IP_CFG_DEVICEREGISTERS_SW_PATCH_VERSION))
+    #error "Software Version Numbers of Emios_Mcl_Ip_Cfg_Defines.h and Emios_Mcl_Ip_Cfg_DeviceRegisters.h are different"
+#endif
+
+#ifndef DISABLE_MCAL_INTERMODULE_ASR_CHECK
+    /* Check if this header file and Emios_Pwm_Ip_CfgDefines.h file are of the same Autosar version */
+    #if ((EMIOS_MCL_IP_CFG_DEFINES_AR_RELEASE_MAJOR_VERSION != EMIOS_PWM_IP_CFGDEFINES_AR_RELEASE_MAJOR_VERSION) || \
+        (EMIOS_MCL_IP_CFG_DEFINES_AR_RELEASE_MINOR_VERSION != EMIOS_PWM_IP_CFGDEFINES_AR_RELEASE_MINOR_VERSION))
+        #error "AutoSar Version Numbers of Emios_Mcl_Ip_Cfg_Defines.h and Emios_Pwm_Ip_CfgDefines.h are different"
+    #endif
+#endif
+/*==================================================================================================
+*                                            CONSTANTS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                       DEFINES AND MACROS
+==================================================================================================*/
+/* Timer width */
+#define EMIOS_MCL_IP_24BITS_TIMER_WIDTH       STD_OFF
+
+
+
+
+/*-----------------------------------------------/
+/  EMIOS HW MASTER BUSES                        /
+/-----------------------------------------------*/
+#define EMIOS_CH_0                        ((uint16)0U)
+#define EMIOS_CH_8                        ((uint16)8U)
+#define EMIOS_CH_16                        ((uint16)16U)
+#define EMIOS_CH_22                        ((uint16)22U)
+#define EMIOS_CH_23                        ((uint16)23U)
+
+/* Macros that indicate EMIOS channels used by MCL. */
+#ifndef EMIOS_0_CH_0_USED
+    #define EMIOS_0_CH_0_USED
+#else
+    #error "EMIOS_0_CH_0 channel cannot be used by MCL driver. Channel locked by other driver!"
+#endif
+
+/* Macros used to save logic MCL EMIOS channel encoding. */
+#define MCL_EMIOS_LOGIC_CH0     (uint16)((0U << 8U) + EMIOS_CH_0)
+
+#define EMIOS_MCL_IP_DEV_ERROR_DETECT      (STD_OFF)
+
+/** @brief Disable EMIOS multicore support at IP layer */
+#define EMIOS_IP_MULTICORE_IS_AVAILABLE     (STD_OFF)
+
+
+
+/*==================================================================================================
+*                                              ENUMS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                  STRUCTURES AND OTHER TYPEDEFS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                  GLOBAL VARIABLE DECLARATIONS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                       FUNCTION PROTOTYPES
+==================================================================================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* EMIOS_MCL_IP_CFG_DEFINES_H */
+
---
+++ s32/soc/s32k344/include/Emios_Pwm_Ip_Cfg.h
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2023 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef EMIOS_PWM_IP_CFG_H
+#define EMIOS_PWM_IP_CFG_H
+
+/**
+*   @file       Emios_Pwm_Ip_Cfg.h
+*
+*   @addtogroup emios_pwm_ip Emios Pwm IPL
+*   @{
+*/
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+
+/*==================================================================================================
+*                                          INCLUDE FILES
+* 1) system and project includes
+* 2) needed interfaces from external units
+* 3) internal and external interfaces from this unit
+==================================================================================================*/
+/* Standard Types */
+#include "Std_Types.h"
+
+/* DevAssert implementation */
+#include "Devassert.h"
+
+/* Register definitions */
+#include "S32K344_EMIOS.h"
+
+/* Include all variants header files. */
+#include "Emios_Pwm_Ip_Init_PBcfg.h"
+
+/*==================================================================================================
+*                                 SOURCE FILE VERSION INFORMATION
+==================================================================================================*/
+#define EMIOS_PWM_IP_CFG_VENDOR_ID                    43
+#define EMIOS_PWM_IP_CFG_MODULE_ID                    121
+#define EMIOS_PWM_IP_CFG_AR_RELEASE_MAJOR_VERSION     4
+#define EMIOS_PWM_IP_CFG_AR_RELEASE_MINOR_VERSION     7
+#define EMIOS_PWM_IP_CFG_AR_RELEASE_REVISION_VERSION  0
+#define EMIOS_PWM_IP_CFG_SW_MAJOR_VERSION             3
+#define EMIOS_PWM_IP_CFG_SW_MINOR_VERSION             0
+#define EMIOS_PWM_IP_CFG_SW_PATCH_VERSION             0
+
+/*==================================================================================================
+*                                       FILE VERSION CHECKS
+==================================================================================================*/
+#ifndef DISABLE_MCAL_INTERMODULE_ASR_CHECK
+    /* Check if header file and Std_Types.h are of the same AUTOSAR version */
+    #if ((EMIOS_PWM_IP_CFG_AR_RELEASE_MAJOR_VERSION != STD_AR_RELEASE_MAJOR_VERSION) || \
+         (EMIOS_PWM_IP_CFG_AR_RELEASE_MINOR_VERSION != STD_AR_RELEASE_MINOR_VERSION))
+        #error "AUTOSAR Version Numbers of Emios_Pwm_Ip_Cfg.h and Std_Types.h are different"
+    #endif
+
+    /* Check if header file and Devassert.h are of the same AUTOSAR version */
+    #if ((EMIOS_PWM_IP_CFG_AR_RELEASE_MAJOR_VERSION != DEVASSERT_AR_RELEASE_MAJOR_VERSION) || \
+         (EMIOS_PWM_IP_CFG_AR_RELEASE_MINOR_VERSION != DEVASSERT_AR_RELEASE_MINOR_VERSION))
+        #error "AUTOSAR Version Numbers of Emios_Pwm_Ip_Cfg.h and Devassert.h are different"
+    #endif
+#endif
+
+    /* Check if header file and Emios_Pwm_Ip_Init_Pbcfg header file are of the same vendor */
+#if (EMIOS_PWM_IP_CFG_VENDOR_ID != EMIOS_PWM_IP_INIT_PB_CFG_VENDOR_ID)
+    #error "Vendor IDs of Emios_Pwm_Ip_Cfg.h and Emios_Pwm_Ip_Init_Pbcfg.h are different."
+#endif
+
+/* Check if header file and Pwm_EnvCfg header file are of the same AUTOSAR version */
+#if ((EMIOS_PWM_IP_CFG_AR_RELEASE_MAJOR_VERSION    != EMIOS_PWM_IP_INIT_PB_CFG_AR_RELEASE_MAJOR_VERSION) || \
+     (EMIOS_PWM_IP_CFG_AR_RELEASE_MINOR_VERSION    != EMIOS_PWM_IP_INIT_PB_CFG_AR_RELEASE_MINOR_VERSION) || \
+     (EMIOS_PWM_IP_CFG_AR_RELEASE_REVISION_VERSION != EMIOS_PWM_IP_INIT_PB_CFG_AR_RELEASE_REVISION_VERSION))
+    #error "AUTOSAR version numbers of Emios_Pwm_Ip_Cfg.h and Emios_Pwm_Ip_Init_Pbcfg.h are different."
+#endif
+
+/* Check if header file and Pwm_EnvCfg header file are of the same software version */
+#if ((EMIOS_PWM_IP_CFG_SW_MAJOR_VERSION != EMIOS_PWM_IP_INIT_PB_CFG_SW_MAJOR_VERSION) || \
+     (EMIOS_PWM_IP_CFG_SW_MINOR_VERSION != EMIOS_PWM_IP_INIT_PB_CFG_SW_MINOR_VERSION) || \
+     (EMIOS_PWM_IP_CFG_SW_PATCH_VERSION != EMIOS_PWM_IP_INIT_PB_CFG_SW_PATCH_VERSION))
+    #error "Software version numbers of Emios_Pwm_Ip_Cfg.h and Emios_Pwm_Ip_Init_Pbcfg.h are different."
+#endif
+
+/*==================================================================================================
+*                                            CONSTANTS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                       DEFINES AND MACROS
+==================================================================================================*/
+/** @brief      Enable the Emios Ip */
+#define EMIOS_PWM_IP_USED                       (STD_OFF)
+
+/** @brief      Switch to enable the development error detection. */
+#define EMIOS_PWM_IP_DEV_ERROR_DETECT           (STD_OFF)
+
+/** @brief      The number of Emios instances available on platform */
+#define EMIOS_PWM_IP_INSTANCE_COUNT                (eMIOS_INSTANCE_COUNT)
+/** @brief      The number of channels available on each Emios instance */
+#define EMIOS_PWM_IP_CHANNEL_COUNT                 (eMIOS_CH_UC_UC_COUNT)
+
+/** @brief      The number of PWM modes implemented in hardware */
+#define EMIOS_PWM_IP_MODES                         (5U)
+/** @brief      Mapping between available channels and the PWM mode supported by each of them */
+#define EMIOS_PWM_IP_CHANNEL_MODES                 {\
+                                                    {0x00C101FFUL, 0x000000FEUL, 0x00FFFFFFUL, 0x00FFFFFFUL, 0x0000FEFEUL},\
+                                                    {0x00C10101UL, 0x00000000UL, 0x00FFFFFFUL, 0x00FFFFFFUL, 0x0000FEFEUL},\
+                                                    {0x00C10101UL, 0x00000000UL, 0x00FFFFFFUL, 0x00FFFFFFUL, 0x0000FEFEUL},\
+                                                }
+/** @brief Min counter value for PWM modes */
+#define EMIOS_PWM_IP_MIN_CNT_VAL                   (0x0001UL)
+/** @brief Max counter value for PWM modes */
+#define EMIOS_PWM_IP_MAX_CNT_VAL                   (0xFFFFUL)
+
+/** @brief      Counter bus channel number */
+#define EMIOS_PWM_IP_COUNTER_BUS_A                 (23U)
+#define EMIOS_PWM_IP_COUNTER_BUS_BCDE              (248U)
+#define EMIOS_PWM_IP_COUNTER_BUS_F                 (22U)
+
+
+#define EMIOS_PWM_IP_NUM_OF_CHANNELS_USED       ((uint8)U)
+#define EMIOS_PWM_IP_INITIAL_MODES        \
+{\
+    }
+
+/*==================================================================================================
+*                                              ENUMS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                  STRUCTURES AND OTHER TYPEDEFS
+==================================================================================================*/
+/* Redefine eMIOS_Type from header file to comply with coding guidelines */
+typedef eMIOS_Type Emios_Pwm_Ip_HwAddrType;
+/**
+* @brief        PWM Period type (the value of the period is platform dependent and thus configurable)
+*/
+typedef uint16 Emios_Pwm_Ip_PeriodType;
+
+/**
+* @brief        PWM Duty type (the value of the period is platform dependent and thus configurable)
+*/
+typedef uint16 Emios_Pwm_Ip_DutyType;
+
+/*==================================================================================================
+*                                  GLOBAL VARIABLE DECLARATIONS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                       FUNCTION PROTOTYPES
+==================================================================================================*/
+
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
+
+#endif /* EMIOS_PWM_IP_CFG_H */
+
