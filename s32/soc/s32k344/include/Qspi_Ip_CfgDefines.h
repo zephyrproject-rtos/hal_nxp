bda6119e5aaa1fc1e30c4b9eeb76ff66e7628ffb
Manuel Argüelles <manuel.arguelles@nxp.com>
2023-07-07 13:28:50 -0300

    s32: soc: s32k3: add QSPI soc specific code
    Code autogenerated with S32 Design Studio for S32 Platform for S32K344.
    
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

---
+++ s32/soc/s32k344/include/Qspi_Ip_CfgDefines.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2023 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef QSPI_IP_CFG_DEFINES_H
+#define QSPI_IP_CFG_DEFINES_H
+
+/**
+*   @file Qspi_Ip_CfgDefines.h
+*
+*   @addtogroup FLS
+*   @{
+*/
+
+/* implements Qspi_Ip_CfgDefines.h_Artifact */
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+
+/*==================================================================================================
+*                                        INCLUDE FILES
+* 1) system and project includes
+* 2) needed interfaces from external units
+* 3) internal and external interfaces from this unit
+==================================================================================================*/
+
+/*==================================================================================================
+*                              SOURCE FILE VERSION INFORMATION
+==================================================================================================*/
+#define QSPI_IP_VENDOR_ID_CFG_DEFINES                    43
+#define QSPI_IP_AR_RELEASE_MAJOR_VERSION_CFG_DEFINES     4
+#define QSPI_IP_AR_RELEASE_MINOR_VERSION_CFG_DEFINES     7
+#define QSPI_IP_AR_RELEASE_REVISION_VERSION_CFG_DEFINES  0
+#define QSPI_IP_SW_MAJOR_VERSION_CFG_DEFINES             3
+#define QSPI_IP_SW_MINOR_VERSION_CFG_DEFINES             0
+#define QSPI_IP_SW_PATCH_VERSION_CFG_DEFINES             0
+
+/*==================================================================================================
+*                                     FILE VERSION CHECKS
+==================================================================================================*/
+
+/*==================================================================================================
+                                       DEFINES AND MACROS
+==================================================================================================*/
+
+
+#define QSPI_IP_SFP_ENABLE_GLOBAL                    (STD_OFF)
+#define QSPI_IP_SFP_ENABLE_MDAD                      (STD_OFF)
+#define QSPI_IP_SFP_ENABLE_FRAD                      (STD_OFF)
+
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
+
+#endif /* QSPI_IP_CFG_DEFINES_H */
