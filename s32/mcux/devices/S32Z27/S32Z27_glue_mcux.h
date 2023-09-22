098b6032f8b87466fa71c31c6c5ee16452bbcaef
Manuel Argüelles <manuel.arguelles@nxp.com>
2023-09-22 14:15:42 +0700

    s32ze: add PIT definitions for MCUX
    Add RTU.PIT module features and definitions for S32Z27.
    
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

--- s32/mcux/devices/S32Z27/S32Z27_glue_mcux.h
+++ s32/mcux/devices/S32Z27/S32Z27_glue_mcux.h
@@ -7,4 +7,16 @@
 #ifndef _S32Z27_GLUE_MCUX_H_
 #define _S32Z27_GLUE_MCUX_H_
 
+/* PIT - Peripheral instance base addresses */
+/** Peripheral PIT0 base address */
+#define PIT0_BASE                                IP_PIT_0_BASE
+/** Peripheral PIT0 base pointer */
+#define PIT0                                     IP_PIT_0
+/** Array initializer of PIT peripheral base addresses */
+#define PIT_BASE_ADDRS                           IP_PIT_BASE_ADDRS
+/** Array initializer of PIT peripheral base pointers */
+#define PIT_BASE_PTRS                            IP_PIT_BASE_PTRS
+/** Interrupt vectors for the PIT peripheral type */
+#define PIT_IRQS                                 { RTU_RTUn_PIT0_IRQn }
+
 #endif /* _S32Z27_GLUE_MCUX_H_ */
