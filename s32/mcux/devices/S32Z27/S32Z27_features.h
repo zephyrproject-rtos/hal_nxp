6018baee13791aef8845034f25f4f82869eed644
Manuel Argüelles <manuel.arguelles@nxp.com>
2023-09-22 14:15:42 +0700

    s32ze: add PIT definitions for MCUX
    Add RTU.PIT module features and definitions for S32Z27.
    
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

--- s32/mcux/devices/S32Z27/S32Z27_features.h
+++ s32/mcux/devices/S32Z27/S32Z27_features.h
@@ -7,4 +7,22 @@
 #ifndef _S32Z27_FEATURES_H_
 #define _S32Z27_FEATURES_H_
 
+/* SOC module features */
+
+/* @brief PIT availability on the SoC. */
+#define FSL_FEATURE_SOC_PIT_COUNT (2)
+
+/* PIT module features */
+
+/* @brief Number of channels (related to number of registers LDVALn, CVALn, TCTRLn, TFLGn). */
+#define FSL_FEATURE_PIT_TIMER_COUNT (4)
+/* @brief Has lifetime timer (related to existence of registers LTMR64L and LTMR64H). */
+#define FSL_FEATURE_PIT_HAS_LIFETIME_TIMER (1)
+/* @brief Has chain mode (related to existence of register bit field TCTRLn[CHN]). */
+#define FSL_FEATURE_PIT_HAS_CHAIN_MODE (1)
+/* @brief Has shared interrupt handler (has not individual interrupt handler for each channel). */
+#define FSL_FEATURE_PIT_HAS_SHARED_IRQ_HANDLER (1)
+/* @brief Has timer enable control. */
+#define FSL_FEATURE_PIT_HAS_MDIS (1)
+
 #endif /* _S32Z27_FEATURES_H_ */
