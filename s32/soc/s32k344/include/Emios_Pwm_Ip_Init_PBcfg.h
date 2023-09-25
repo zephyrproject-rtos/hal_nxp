4f58cd8976ae00595fa6430f041f2b16c4118287
Dat Nguyen Duy <dat.nguyenduy@nxp.com>
2023-09-25 13:10:58 +0700

    s32: soc: s32k344: use new dt enum value for PWM mode
    For preparing PWM capture support, name of PWM modes
    were changed, update to use the new ones
    
    Signed-off-by: Dat Nguyen Duy <dat.nguyenduy@nxp.com>

--- s32/soc/s32k344/include/Emios_Pwm_Ip_Init_PBcfg.h
+++ s32/soc/s32k344/include/Emios_Pwm_Ip_Init_PBcfg.h
@@ -97,25 +97,25 @@ extern "C"{
 
 #define EMIOS_OPWFMB_MODE_USED(node_id)                                                    \
             COND_CODE_1(DT_NODE_HAS_PROP(node_id, pwm_mode),                               \
-                (DT_ENUM_HAS_VALUE(node_id, pwm_mode, MODE_OPWFMB)),                       \
-                    (0)) ||
+                       (DT_ENUM_HAS_VALUE(node_id, pwm_mode, OPWFMB)),                     \
+                       (0)) ||
 
 #define IS_EMIOS_OPWFMB_MODE_USED(n)                                                       \
             DT_INST_FOREACH_CHILD_STATUS_OKAY(n, EMIOS_OPWFMB_MODE_USED)
 
 #define EMIOS_OPWMB_MODE_USED(node_id)                                                     \
             COND_CODE_1(DT_NODE_HAS_PROP(node_id, pwm_mode),                               \
-                (DT_ENUM_HAS_VALUE(node_id, pwm_mode, MODE_OPWMB)),                        \
-                    (0)) ||
+                       (DT_ENUM_HAS_VALUE(node_id, pwm_mode, OPWMB)),                      \
+                       (0)) ||
 
 #define IS_EMIOS_OPWMB_MODE_USED(n)                                                        \
             DT_INST_FOREACH_CHILD_STATUS_OKAY(n, EMIOS_OPWMB_MODE_USED)
 
 #define EMIOS_OPWMCB_MODE_USED(node_id)                                                    \
             COND_CODE_1(DT_NODE_HAS_PROP(node_id, pwm_mode),                               \
-                (DT_ENUM_HAS_VALUE(node_id, pwm_mode, MODE_OPWMCB_TRAIL_EDGE) ||           \
-                 DT_ENUM_HAS_VALUE(node_id, pwm_mode, MODE_OPWMCB_LEAD_EDGE)               \
-                ),(0)) ||
+                       (DT_ENUM_HAS_VALUE(node_id, pwm_mode, OPWMCB_TRAIL_EDGE) ||         \
+                        DT_ENUM_HAS_VALUE(node_id, pwm_mode, OPWMCB_LEAD_EDGE)             \
+                        ),(0)) ||
 
 #define IS_EMIOS_OPWMCB_MODE_USED(n)                                                       \
             DT_INST_FOREACH_CHILD_STATUS_OKAY(n, EMIOS_OPWMCB_MODE_USED)
