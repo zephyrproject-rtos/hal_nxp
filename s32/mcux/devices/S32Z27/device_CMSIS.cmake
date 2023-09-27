3ca5c1eece0102f4675c490225625c92ab235173
Manuel Arguelles <manuel.arguelles@nxp.com>
2023-09-27 13:41:15 +0700

    s32ze: support building with MCUX drivers
    Introduce necessary glue code and definitions to build MCUX with S32Z27
    devices so that we can leverage existing shim drivers for common NXP hw
    blocks.
    
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

---
+++ s32/mcux/devices/S32Z27/device_CMSIS.cmake
@@ -0,0 +1,10 @@
+include_guard(GLOBAL)
+message("device_CMSIS component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    # nothing to build
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/.
+)
