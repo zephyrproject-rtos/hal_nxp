4efff442616503f15bf05f7d3f6c4544ca60acfa
Manuel Arguelles <manuel.arguelles@nxp.com>
2023-09-27 13:41:15 +0700

    s32ze: support building with MCUX drivers
    Introduce necessary glue code and definitions to build MCUX with S32Z27
    devices so that we can leverage existing shim drivers for common NXP hw
    blocks.
    
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

---
+++ s32/mcux/devices/S32Z27/S32Z27_features.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2023 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _S32Z27_FEATURES_H_
+#define _S32Z27_FEATURES_H_
+
+#endif /* _S32Z27_FEATURES_H_ */
---
+++ s32/mcux/devices/S32Z27/S32Z27_glue_mcux.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2023 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _S32Z27_GLUE_MCUX_H_
+#define _S32Z27_GLUE_MCUX_H_
+
+#endif /* _S32Z27_GLUE_MCUX_H_ */
