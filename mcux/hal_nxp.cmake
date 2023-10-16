796131b04b1bdca726df4798f7ff8aa56a0b8205
Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>
2023-10-16 13:37:03 +0300

    mcux: hal_nxp: Compile SCFW API for QM and QXP's DSP core
    This commit adds support for compiling the SCFW API
    in Zephyr for i.MX8QM and i.MX8QXP's DSP core.
    
    Signed-off-by: Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>

--- mcux/hal_nxp.cmake
+++ mcux/hal_nxp.cmake
@@ -53,7 +53,7 @@ zephyr_library_compile_definitions_ifdef(
 
 
 # Required by all SCFW-based SoCs
-if (CONFIG_SOC_MIMX8QM_A53)
+if (CONFIG_SOC_MIMX8QM_A53 OR CONFIG_SOC_MIMX8QM_ADSP OR CONFIG_SOC_MIMX8QXP_ADSP)
     list(APPEND CMAKE_MODULE_PATH
         ${CMAKE_CURRENT_LIST_DIR}/mcux-sdk/devices/${MCUX_DEVICE}/scfw_api
     )
