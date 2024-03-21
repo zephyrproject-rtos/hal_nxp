937a344880fe0ef5a494b25c811ca039eb1ce008
Mahesh Mahadevan <mahesh.mahadevan@nxp.com>
2024-03-21 13:16:07 -0500

    cmake: Add SoC DMA file for MCXN947
    Add the SoC DMA cmake file for MCXN947
    
    Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

---
+++ mcux/mcux-sdk/devices/MCXN947/drivers/driver_edma_soc.cmake
@@ -0,0 +1,15 @@
+#Description: EDMA SOC Driver; user_visible: True
+include_guard(GLOBAL)
+message("driver_edma_soc component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/fsl_edma_soc.c
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/.
+)
+
+
+include(driver_common)
+include(driver_edma4)
