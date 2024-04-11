f7b7db9a5cc7e573dbcf1bf8cb9e299d5aef500e
Mark Wang <yichang.wang@nxp.com>
2024-04-11 20:05:29 +0800

    mcux: hal_nxp: support udc_mucx driver
     - include middleware_usb_device_ehci or middleware_usb_device_ip3511fs
    when controller is enabled in Zephyr.
    
    Signed-off-by: Mark Wang <yichang.wang@nxp.com>

--- mcux/hal_nxp.cmake
+++ mcux/hal_nxp.cmake
@@ -362,3 +362,16 @@ if (CONFIG_USB_DEVICE_DRIVER)
   zephyr_include_directories(${CMAKE_CURRENT_LIST_DIR}/middleware/mcux-sdk-middleware-usb/phy)
   zephyr_include_directories(${CMAKE_CURRENT_LIST_DIR}/middleware/mcux-sdk-middleware-usb/include)
 endif()
+
+if (CONFIG_UDC_DRIVER)
+  list(APPEND CMAKE_MODULE_PATH
+    ${CMAKE_CURRENT_LIST_DIR}/middleware/mcux-sdk-middleware-usb
+  )
+  include_ifdef(CONFIG_USB_UDC_NXP_PHY          middleware_usb_phy)
+  include_ifdef(CONFIG_USB_UDC_NXP_EHCI         middleware_usb_device_ehci)
+  include_ifdef(CONFIG_USB_UDC_NXP_IP3511       middleware_usb_device_ip3511fs)
+
+  zephyr_include_directories(${CMAKE_CURRENT_LIST_DIR}/middleware/mcux-sdk-middleware-usb/device)
+  zephyr_include_directories(${CMAKE_CURRENT_LIST_DIR}/middleware/mcux-sdk-middleware-usb/phy)
+  zephyr_include_directories(${CMAKE_CURRENT_LIST_DIR}/middleware/mcux-sdk-middleware-usb/include)
+endif()
