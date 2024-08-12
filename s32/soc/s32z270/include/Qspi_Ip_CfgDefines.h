652fee9c86d69e9810afb0284de9b5d0da28cc3e
Cong Nguyen Huu <cong.nguyenhuu@nxp.com>
2024-08-12 14:08:49 +0700

    s32: soc: s32z2: add QSPI soc specific code
    Code autogenerated with S32 Design Studio for s32ze
    
    Signed-off-by: Cong Nguyen Huu <cong.nguyenhuu@nxp.com>

---
+++ s32/soc/s32z270/include/Qspi_Ip_CfgDefines.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2024 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef QSPI_IP_CFG_DEFINES_H
+#define QSPI_IP_CFG_DEFINES_H
+
+/**
+*   @file Qspi_Ip_CfgDefines.h
+*
+*   @addtogroup IPV_QSPI
+*   Qspi_Ip_CfgDefines.h_Artifact
+*   @{
+*/
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+
+/*==================================================================================================
+*                                        INCLUDE FILES
+* 1) system and project includes
+* 2) needed interfaces from external units
+* 3) internal and external interfaces from this unit
+==================================================================================================*/
+
+/*==================================================================================================
+*                              SOURCE FILE VERSION INFORMATION
+==================================================================================================*/
+#define QSPI_IP_VENDOR_ID_CFG_DEFINES                    43
+#define QSPI_IP_AR_RELEASE_MAJOR_VERSION_CFG_DEFINES     4
+#define QSPI_IP_AR_RELEASE_MINOR_VERSION_CFG_DEFINES     7
+#define QSPI_IP_AR_RELEASE_REVISION_VERSION_CFG_DEFINES  0
+#define QSPI_IP_SW_MAJOR_VERSION_CFG_DEFINES             2
+#define QSPI_IP_SW_MINOR_VERSION_CFG_DEFINES             0
+#define QSPI_IP_SW_PATCH_VERSION_CFG_DEFINES             0
+
+/*==================================================================================================
+*                                     FILE VERSION CHECKS
+==================================================================================================*/
+
+/*==================================================================================================
+                                       DEFINES AND MACROS
+==================================================================================================*/
+
+
+#define QSPI_IP_SFP_ENABLE_GLOBAL                    (STD_ON)
+#define QSPI_IP_SFP_ENABLE_MDAD                      (STD_ON)
+#define QSPI_IP_SFP_ENABLE_FRAD                      (STD_ON)
+
+
+/* Enable Multicore support when using MemAcc*/
+#define QSPI_IP_MULTICORE_ENABLED                        (STD_OFF)
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
+
+#endif /* QSPI_IP_CFG_DEFINES_H */
