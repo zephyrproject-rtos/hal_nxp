bf0a2d8bc7ba3edf22622bc57a2be75128c2d97d
M. Elia <marco.elia@ideasandmotion.com>
2024-11-09 09:41:17 +0100

    added support for S32K148

---
+++ s32/mcux/devices/S32K148/drivers/fsl_clock.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2023 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _FSL_CLOCK_H_
+#define _FSL_CLOCK_H_
+
+#include "fsl_common.h"
+
+#endif /* _FSL_CLOCK_H_ */
