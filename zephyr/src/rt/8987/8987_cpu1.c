8e300e6ca9267d9151851bf7074d75b46ea5ffaf
Abhinav Kulkarni <abhinav.kulkarni@nxp.com>
2025-01-14 18:28:10 +0530

    hal_nxp: zephyr: src: rt: Added src for fw blobs.
    Added support for IW416 and NW61x firmware blobs supporting files.
    
    Signed-off-by: Abhinav Kulkarni <abhinav.kulkarni@nxp.com>

---
+++ zephyr/src/rt/8987/8987_cpu1.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2023-2025 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+
+#include <stdint.h>
+
+const uint8_t fw_cpu1[] = {
+    #include <8987_wifi_fw.bin.inc>
+};
+
+const unsigned char *wlan_fw_bin = (const unsigned char *)(void *)&fw_cpu1[0];
+const unsigned int wlan_fw_bin_len = sizeof(fw_cpu1);
