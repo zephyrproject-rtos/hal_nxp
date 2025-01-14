11a5260b53ce93289aec5b3858c2f023d600851f
Abhinav Kulkarni <abhinav.kulkarni@nxp.com>
2025-01-14 18:28:10 +0530

    hal_nxp: zephyr: src: rt: Added src for fw blobs.
    Added support for IW416 and NW61x firmware blobs supporting files.
    
    Signed-off-by: Abhinav Kulkarni <abhinav.kulkarni@nxp.com>

---
+++ zephyr/src/rt/IW416/IW416_cpu2.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2023-2025 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+
+#include <stdint.h>
+
+const uint8_t fw_cpu2_ble[] = {
+    #include <iw416_ble_fw.bin.inc>
+};
+
+const unsigned char *bt_fw_bin = (const unsigned char *)(void *)&fw_cpu2_ble[0];
+const unsigned int bt_fw_bin_len = sizeof(fw_cpu2_ble);
