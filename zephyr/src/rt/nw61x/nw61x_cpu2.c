bf00ba1f376535e594876bc743b4d1b6b61f7694
Abhinav Kulkarni <abhinav.kulkarni@nxp.com>
2025-02-13 19:25:35 +0530

    hal_nxp: zephyr: src: rt: Enabling IW416 and IW612 NXP Wireless SoCs.
    Added support for IW416 and NW61x socs for BT and Wi-Fi.
    
    Signed-off-by: Abhinav Kulkarni <abhinav.kulkarni@nxp.com>

--- zephyr/src/rt/nw61x/nw61x_cpu2.c
+++ zephyr/src/rt/nw61x/nw61x_cpu2.c
@@ -9,9 +9,9 @@
 
 #include <stdint.h>
 
-const uint8_t fw_cpu2_ble[] = {
-    #include <nw61x_ble_fw.bin.inc>
+const uint8_t fw_cpu2_bt[] = {
+    #include <nw61x_bt_fw.bin.inc>
 };
 
-const unsigned char *bt_fw_bin = (const unsigned char *)(void *)&fw_cpu2_ble[0];
-const unsigned int bt_fw_bin_len = sizeof(fw_cpu2_ble);
+const unsigned char *bt_fw_bin = (const unsigned char *)(void *)&fw_cpu2_bt[0];
+const unsigned int bt_fw_bin_len = sizeof(fw_cpu2_bt);
