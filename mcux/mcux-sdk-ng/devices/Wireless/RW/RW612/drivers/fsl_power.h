ba6f9fb6ffdee0d454c896a9a4d84beaee91de8d
Feng Wei <wei.feng@nxp.com>
2025-02-28 17:08:40 +0800

    Added new SVC trim equation for new samples
    Signed-off-by: Feng Wei <wei.feng@nxp.com>

--- mcux/mcux-sdk-ng/devices/Wireless/RW/RW612/drivers/fsl_power.h
+++ mcux/mcux-sdk-ng/devices/Wireless/RW/RW612/drivers/fsl_power.h
@@ -23,8 +23,8 @@
 
 /*! @name Driver version */
 /*@{*/
-/*! @brief POWER driver version 2.5.0. */
-#define FSL_POWER_DRIVER_VERSION (MAKE_VERSION(2, 5, 0))
+/*! @brief POWER driver version 2.5.1. */
+#define FSL_POWER_DRIVER_VERSION (MAKE_VERSION(2, 5, 1))
 /*@}*/
 
 /*!
