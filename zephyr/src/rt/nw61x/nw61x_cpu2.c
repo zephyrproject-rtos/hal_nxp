601e1e538abaf9f5985e28531b3b6a2a07e38d2a
Abhinav Kulkarni <abhinav.kulkarni@nxp.com>
2025-04-03 15:44:57 +0530

    Zephyr: Src: Added files for IW416 and IW612 SoCs to work
    Adding support for IW416 and IW612 socs.
    
    Signed-off-by: Abhinav Kulkarni <abhinav.kulkarni@nxp.com>

---
+++ zephyr/src/rt/nw61x/nw61x_cpu2.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2023-2025 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+
+#include <stdint.h>
+
+const uint8_t fw_cpu2_bt[] = {
+#ifndef CONFIG_BUILD_ONLY_NO_BLOBS
+    #include <nw61x_bt_fw.bin.inc>
+#endif /*CONFIG_BUILD_ONLY_NO_BLOBS*/
+};
+
+const unsigned char *bt_fw_bin = (const unsigned char *)(void *)&fw_cpu2_bt[0];
+const unsigned int bt_fw_bin_len = sizeof(fw_cpu2_bt);
