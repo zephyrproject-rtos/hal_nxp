afb44bde9a76e4c2f463d482810d7108fd6ae40e
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2025-07-21 13:39:34 +0800

    hal_nxp: Sync 25.06 boards to hal_nxp
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

--- mcux/mcux-sdk-ng/boards/mimxrt700evk/flash_config/flash_config.c
+++ mcux/mcux-sdk-ng/boards/mimxrt700evk/flash_config/flash_config.c
@@ -1,6 +1,5 @@
 /*
- * Copyright 2023 NXP
- * All rights reserved.
+ * Copyright 2023, 2025 NXP
  *
  * SPDXLicense-Identifier: BSD-3-Clause
  */
@@ -48,6 +47,11 @@ const fc_static_platform_config_t flash_config = {
             .sflashA1Size    = 64ul * 1024u * 1024u,
             .busyOffset      = 0u,
             .busyBitPolarity = 0u,
+#if defined(FSL_FEATURE_SILICON_VERSION_A)
+            .lutCustomSeqEnable = 0u,
+#else
+            .lutCustomSeqEnable = 1u,
+#endif
             .lookupTable =
             {
                 /*Read*/
@@ -95,7 +99,7 @@ const fc_static_platform_config_t flash_config = {
                     FC_XSPI_LUT_SEQ(FC_CMD_SDR, FC_XSPI_1PAD, 0x00, FC_CMD_SDR, FC_XSPI_1PAD, 0x00),
                 [5 * 6 + 2] =
                     FC_XSPI_LUT_SEQ(FC_CMD_SDR, FC_XSPI_1PAD, 0x00, FC_CMD_WRITE_SDR,  FC_XSPI_1PAD, 0x01),
-
+#if defined(FSL_FEATURE_SILICON_VERSION_A)
                 /* Page program */
                 [5 * 9 + 0] =
                     FC_XSPI_LUT_SEQ(FC_CMD_DDR, FC_XSPI_8PAD, 0x12, FC_CMD_DDR, FC_XSPI_8PAD, 0xED),
@@ -106,6 +110,25 @@ const fc_static_platform_config_t flash_config = {
                 [5 * 11 + 0] =
                     FC_XSPI_LUT_SEQ(FC_CMD_DDR, FC_XSPI_8PAD, 0x60, FC_CMD_DDR, FC_XSPI_8PAD, 0x9F),
             },
+#else
+                /* Page program */
+                [5 * 9 + 0] = 
+                    FC_XSPI_LUT_SEQ(FC_CMD_DDR, FC_XSPI_8PAD, 0x06, FC_CMD_DDR, FC_XSPI_8PAD, 0xF9),  
+                [5 * 9 + 1] =
+                  FC_XSPI_LUT_SEQ(FC_CMD_JUMP_TO_SEQ, FC_XSPI_8PAD, 0x2U, FC_CMD_STOP, FC_XSPI_8PAD, 0x0),
+                [5 * 10 + 0] =
+                    FC_XSPI_LUT_SEQ(FC_CMD_DDR, FC_XSPI_8PAD, 0x12, FC_CMD_DDR, FC_XSPI_8PAD, 0xED),
+                [5 * 10 + 1] =
+                    FC_XSPI_LUT_SEQ(FC_CMD_RADDR_DDR, FC_XSPI_8PAD, 0x20, FC_CMD_WRITE_DDR, FC_XSPI_8PAD, 0x4),
+                    /* Erase Chip */
+                [5 * 13 + 0] =
+                    FC_XSPI_LUT_SEQ(FC_CMD_DDR, FC_XSPI_8PAD, 0x60, FC_CMD_DDR, FC_XSPI_8PAD, 0x9F),
+            },
+
+            .lutCustomSeq[4].seqNum = 2U, /* For PAGEPROGRAM custom LUT, uses joined LUT. */
+            .lutCustomSeq[4].seqId = 9U,
+#endif
+
         },
         .pageSize           = 256u,
         .sectorSize         = 4u * 1024u,
