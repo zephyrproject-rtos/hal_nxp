455abbda0e2faa53086ad883ce843957f871ddc2
Hui Bai <hui.bai@nxp.com>
2025-08-11 10:46:23 +0800

    mcux: wifi_nxp: Add TLSv1.3 prerequisites in mbedtls config files
    Added and enable below flags in mbedtls config files:
    PSA_WANT_ALG_HKDF_EXTRACT
    PSA_WANT_ALG_HKDF_EXPAND
    MBEDTLS_PSA_BUILTIN_ALG_HKDF_EXTRACT
    MBEDTLS_PSA_BUILTIN_ALG_HKDF_EXPAND
    
    Signed-off-by: Hui Bai <hui.bai@nxp.com>

--- mcux/middleware/wifi_nxp/incl/port/mbedtls/wpa_supp_dcp_mbedtls_config.h
+++ mcux/middleware/wifi_nxp/incl/port/mbedtls/wpa_supp_dcp_mbedtls_config.h
@@ -3782,6 +3782,10 @@
 #define PSA_WANT_ALG_SHA_256                    1
 #define PSA_WANT_ALG_SHA_384                    1
 #define PSA_WANT_ALG_SHA_512                    1
+#define PSA_WANT_ALG_HKDF_EXTRACT               1
+#define PSA_WANT_ALG_HKDF_EXPAND                1
+#define MBEDTLS_PSA_BUILTIN_ALG_HKDF_EXTRACT    1
+#define MBEDTLS_PSA_BUILTIN_ALG_HKDF_EXPAND     1
 //#define PSA_WANT_ALG_STREAM_CIPHER              1
 #define PSA_WANT_ALG_TLS12_PRF                  1
 #define PSA_WANT_ALG_TLS12_PSK_TO_MS            1
