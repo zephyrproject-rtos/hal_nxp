5955d17c629a44513a16bb280d18ee1fed821327
Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
2025-08-27 15:47:53 +0200

    mcux: connectivity_framework: remove connected_mcu and switch to wireless_mcu
    connected_mcu platform was renamed to wireless_mcu, we can switch to
    it and remove the old folder.
    
    Signed-off-by: Yassine El Aissaoui <yassine.elaissaoui@nxp.com>

--- mcux/middleware/mcux-sdk-middleware-connectivity-framework/connectivity_framework.cmake
+++ mcux/middleware/mcux-sdk-middleware-connectivity-framework/connectivity_framework.cmake
@@ -37,19 +37,24 @@ endif()
 
 if(CONFIG_SOC_FAMILY_MCXW)
     target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
-        ${CMAKE_CURRENT_LIST_DIR}/platform/connected_mcu/fwk_platform.c
-        ${CMAKE_CURRENT_LIST_DIR}/platform/connected_mcu/fwk_platform_ics.c
-        ${CMAKE_CURRENT_LIST_DIR}/platform/connected_mcu/fwk_platform_ot.c
-        ${CMAKE_CURRENT_LIST_DIR}/platform/connected_mcu/fwk_platform_ble.c
+        ${CMAKE_CURRENT_LIST_DIR}/platform/wireless_mcu/fwk_platform.c
+        ${CMAKE_CURRENT_LIST_DIR}/platform/wireless_mcu/fwk_platform_ics.c
+        ${CMAKE_CURRENT_LIST_DIR}/platform/wireless_mcu/fwk_platform_ot.c
+        ${CMAKE_CURRENT_LIST_DIR}/platform/wireless_mcu/fwk_platform_ble.c
     )
+
+    if(CONFIG_SOC_MCXW716C)
+        set(platform "kw45_k32w1_mcxw71")
+    elseif(CONFIG_SOC_MCXW727C_CPU0)
+        set(platform "kw47_mcxw72")
+    endif()
 
     zephyr_include_directories(
         ${CMAKE_CURRENT_LIST_DIR}/Common
-        ${CMAKE_CURRENT_LIST_DIR}/platform/connected_mcu
-        ${CMAKE_CURRENT_LIST_DIR}/platform/connected_mcu/configs
+        ${CMAKE_CURRENT_LIST_DIR}/platform/wireless_mcu
+        ${CMAKE_CURRENT_LIST_DIR}/platform/${platform}
+        ${CMAKE_CURRENT_LIST_DIR}/platform/${platform}/configs
     )
-
-    zephyr_compile_definitions_ifdef(CONFIG_SOC_MCXW727C FWK_KW47_MCXW72_FAMILIES=1)
 
     if(DEFINED CONFIG_SOC_SDKNG_UNSUPPORTED)
         include(set_component_osa)
