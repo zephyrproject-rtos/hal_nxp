5d438374998c36cadbb7ed08c60e750f9da1f0e6
Yangbo Lu <yangbo.lu@nxp.com>
2025-08-27 17:37:19 +0800

    mcux-sdk-ng: drivers: netc: get right size of TX timestamp response frame
    Got right size of TX timestamp response frame.
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- mcux/mcux-sdk-ng/drivers/netc/fsl_netc_endpoint.c
+++ mcux/mcux-sdk-ng/drivers/netc/fsl_netc_endpoint.c
@@ -1146,6 +1146,7 @@ status_t EP_GetRxFrameSizeCommon(ep_handle_t *handle, netc_rx_bdr_t *rxBdRing, u
         else
         {
             /* Get Transmit Timestamp Reference Response messages */
+            *length = rxDesc->writeback.bufLen;
             result = kStatus_NETC_RxTsrResp;
         }
     }
