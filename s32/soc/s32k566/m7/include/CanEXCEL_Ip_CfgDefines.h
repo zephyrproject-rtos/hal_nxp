875b03951e98df053305ca3b15cc061c60c14057
Dat Nguyen Duy <dat.nguyenduy@nxp.com>
2026-01-07 16:31:32 +0700

    soc: s32k566: add generated configuration for canxl
    Add generated configuration for CANXL
    
    Signed-off-by: Dat Nguyen Duy <dat.nguyenduy@nxp.com>

---
+++ s32/soc/s32k566/m7/include/CanEXCEL_Ip_CfgDefines.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2024-2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CANEXCEL_IP_CFGDEFINES_H
+#define CANEXCEL_IP_CFGDEFINES_H
+
+/**
+*   @file CanEXCEL_Ip_CfgDefines.h
+*
+*   @addtogroup CanEXCEL
+*   @{
+*/
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/*==================================================================================================
+*                                        INCLUDE FILES
+* 1) system and project includes
+* 2) needed interfaces from external units
+* 3) internal and external interfaces from this unit
+
+==================================================================================================*/
+    #include "S32K566_K566_GPR0.h"
+    #include "S32K566_CANXL.h"
+    #include "S32K566_CANXL_DSC_CONTROL.h"
+    #include "S32K566_CANXL_FILTER_BANK.h"
+    #include "S32K566_CANXL_GRP_CONTROL.h"
+    #include "S32K566_CANXL_MRU.h"
+    #include "S32K566_CANXL_MSG_DESCRIPTORS.h"
+    #include "S32K566_CANXL_RAMECC.h"
+    #include "S32K566_CANXL_RXFIFO.h"
+    #include "S32K566_CANXL_RXFIFO_CONTROL.h"
+    #include "S32K566_CANXL_SIC.h"
+    
+/*==================================================================================================
+*                              SOURCE FILE VERSION INFORMATION
+==================================================================================================*/
+#define CANEXCEL_IP_CFGDEFINES_VENDOR_ID_H                      43
+#define CANEXCEL_IP_CFGDEFINES_AR_RELEASE_MAJOR_VERSION_H       4
+#define CANEXCEL_IP_CFGDEFINES_AR_RELEASE_MINOR_VERSION_H       9
+#define CANEXCEL_IP_CFGDEFINES_AR_RELEASE_REVISION_VERSION_H    0
+#define CANEXCEL_IP_CFGDEFINES_SW_MAJOR_VERSION_H               0
+#define CANEXCEL_IP_CFGDEFINES_SW_MINOR_VERSION_H               8
+#define CANEXCEL_IP_CFGDEFINES_SW_PATCH_VERSION_H               0
+/*
+* @file           CanEXCEL_Ip_CfgDefines.h
+*/
+/*==================================================================================================
+*                                     FILE VERSION CHECKS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                          CONSTANTS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                      DEFINES AND MACROS
+==================================================================================================*/
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+/** @} */
+
+#endif /* CANEXCEL_IP_CFGDEFINES_H */
