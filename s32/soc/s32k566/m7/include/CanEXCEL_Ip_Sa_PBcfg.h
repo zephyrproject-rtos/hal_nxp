8e106b98c91726902b5f4c288358f55a269e5262
Dat Nguyen Duy <dat.nguyenduy@nxp.com>
2026-01-07 16:31:32 +0700

    soc: s32k566: add generated configuration for canxl
    Add generated configuration for CANXL
    
    Signed-off-by: Dat Nguyen Duy <dat.nguyenduy@nxp.com>

---
+++ s32/soc/s32k566/m7/include/CanEXCEL_Ip_Sa_PBcfg.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024-2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CANEXCEL_IP_SA_PBCFG_H
+#define CANEXCEL_IP_SA_PBCFG_H
+/**
+*   @file CanEXCEL_IP_PBcfg.h
+*
+*   @addtogroup CanEXCEL
+*   @{
+*/
+#ifdef __cplusplus
+extern "C"{
+#endif
+/*==================================================================================================
+*                                        INCLUDE FILES
+* 1) system and project includes
+* 2) needed interfaces from external units
+* 3) internal and external interfaces from this unit
+==================================================================================================*/
+
+/*==================================================================================================
+*                              SOURCE FILE VERSION INFORMATION
+==================================================================================================*/
+/*
+* @file           CanEXCEL_Ip_Sa_PBcfg.h
+*/
+#define CANEXCEL_IP_SA_VENDOR_ID_PBCFG_H                     43
+#define CANEXCEL_IP_SA_AR_RELEASE_MAJOR_VERSION_PBCFG_H      4
+#define CANEXCEL_IP_SA_AR_RELEASE_MINOR_VERSION_PBCFG_H      9
+#define CANEXCEL_IP_SA_AR_RELEASE_REVISION_VERSION_PBCFG_H   0
+#define CANEXCEL_IP_SA_SW_MAJOR_VERSION_PBCFG_H              0
+#define CANEXCEL_IP_SA_SW_MINOR_VERSION_PBCFG_H              8
+#define CANEXCEL_IP_SA_SW_PATCH_VERSION_PBCFG_H              0
+/*==================================================================================================
+*                                      DEFINES AND MACROS
+==================================================================================================*/
+#define CANEXCEL_IP_SA_PB_CFG \
+extern const Canexcel_Ip_ConfigType CanXL_Config0; 
+#define INST_CANEXCEL_0 0U
+#define CANEXCEL_IP_SA_STATE_PB_CFG \
+extern Canexcel_Ip_StateType CanEXCEL_State0; 
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+/** @} */
+
+#endif /* CANEXCEL_IP_PBCFG_H */
