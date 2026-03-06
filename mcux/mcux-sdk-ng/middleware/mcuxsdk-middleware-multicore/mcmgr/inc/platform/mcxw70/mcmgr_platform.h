1a1232247e8f6946bb6af1282444496eae699eb3
Axel Le Bourhis <axel.lebourhis@nxp.com>
2026-03-06 12:55:05 +0100

    mcux-sdk-ng: middlware: integrate mcxw70 support in multicore
    Signed-off-by: Axel Le Bourhis <axel.lebourhis@nxp.com>

---
+++ mcux/mcux-sdk-ng/middleware/mcuxsdk-middleware-multicore/mcmgr/inc/platform/mcxw70/mcmgr_platform.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2026 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef MCMGR_PLATFORM_H_
+#define MCMGR_PLATFORM_H_
+
+#include <stdint.h>
+
+/* Count of cores in the system */
+#define MCMGR_CORECOUNT 2
+
+/* Count of memory regions in the system */
+#define MCMGR_MEMREGCOUNT 2
+
+/* MCMGR MU channel index - used for passing startupData */
+#define MCMGR_MU_CHANNEL 3
+
+#endif /* MCMGR_PLATFORM_H_ */
