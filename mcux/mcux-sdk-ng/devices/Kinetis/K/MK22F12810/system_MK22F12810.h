d791df75470f6fe3423be1e2e213fa43cf9d9862
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2026-05-27 13:22:10 +0800

    mcux: sdk-ng: update device/kinetis to 26.06.00 pvw2
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

--- mcux/mcux-sdk-ng/devices/Kinetis/K/MK22F12810/system_MK22F12810.h
+++ mcux/mcux-sdk-ng/devices/Kinetis/K/MK22F12810/system_MK22F12810.h
@@ -129,8 +129,7 @@ void SystemCoreClockUpdate (void);
  *
  * This weak function allows to call specific initialization code during the
  * SystemInit() execution.This can be used when an application specific code needs
- * to be called as close to the reset entry as possible (for example the Multicore
- * Manager MCMGR_EarlyInit() function call).
+ * to be called as close to the reset entry as possible.
  * NOTE: No global r/w variables can be used in this hook function because the
  * initialization of these variables happens after this function.
  */
