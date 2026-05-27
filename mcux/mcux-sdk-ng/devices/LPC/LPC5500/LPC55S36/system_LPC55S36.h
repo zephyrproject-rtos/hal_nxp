df320ea74cba21ea6147a118d2da2e5ca1ecc39e
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2026-05-27 13:29:11 +0800

    mcux: sdk-ng: update device/LPC to 26.06.00 pvw2
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

--- mcux/mcux-sdk-ng/devices/LPC/LPC5500/LPC55S36/system_LPC55S36.h
+++ mcux/mcux-sdk-ng/devices/LPC/LPC5500/LPC55S36/system_LPC55S36.h
@@ -18,7 +18,7 @@
 **         the oscillator (PLL) that is part of the microcontroller device.
 **
 **     Copyright 2016 Freescale Semiconductor, Inc.
-**     Copyright 2016-2025 NXP
+**     Copyright 2016-2026 NXP
 **     SPDX-License-Identifier: BSD-3-Clause
 **
 **     http:                 www.nxp.com
@@ -101,8 +101,7 @@ void SystemCoreClockUpdate (void);
  *
  * This weak function allows to call specific initialization code during the
  * SystemInit() execution.This can be used when an application specific code needs
- * to be called as close to the reset entry as possible (for example the Multicore
- * Manager MCMGR_EarlyInit() function call).
+ * to be called as close to the reset entry as possible.
  * NOTE: No global r/w variables can be used in this hook function because the
  * initialization of these variables happens after this function.
  */
