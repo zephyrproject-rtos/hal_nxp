4a2c4ed33411778c62d93b92deb99639e5407947
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2026-07-03 21:06:35 +0800

    mcux: sdk-ng: update cmake infrastructure to 26.06.00
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

---
+++ mcux/mcux-sdk-ng/arch/arm/configuration/imx.cmake
@@ -0,0 +1,57 @@
+# Copyright 2025-2026 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+if (CONFIG_MCUX_PRJSEG_config.imx.shared)
+    include(${SdkRootDirPath}/arch/arm/configuration/common.cmake)
+endif()
+
+if (CONFIG_MCUX_PRJSEG_config.imx.cortex_a.shared)
+    mcux_add_armgcc_configuration(
+        AS "-D__STARTUP_CLEAR_BSS -D__STARTUP_INITIALIZE_NONCACHEDATA -DGUEST -Wall -Wno-address-of-packed-member -fno-common -ffunction-sections -fdata-sections -ffreestanding -fno-builtin -std=gnu99"
+        CC "-DGUEST -Wall -Wno-address-of-packed-member -MMD -MP -fno-common -ffunction-sections -fdata-sections -ffreestanding -fno-builtin -std=gnu99"
+        CX "-DGUEST -Wall -Wno-address-of-packed-member -MMD -MP -fno-common -ffunction-sections -fdata-sections -ffreestanding -fno-builtin -fno-rtti -fno-exceptions"
+        LD "--specs=nosys.specs -Wall -fno-common -ffunction-sections -fdata-sections -ffreestanding -fno-builtin -Wl,--gc-sections -Wl,-static -Wl,--print-memory-usage"
+        LIB "-lm -lc -lgcc -lnosys"
+    )
+
+    mcux_add_armgcc_configuration(
+        TARGETS ddr_release
+        CC "-O3 -Werror"
+        CX "-O3 -Werror"
+    )
+
+    mcux_add_armgcc_configuration(
+        TARGETS ddr_debug
+        CC "-O0 -g -DDEBUG"
+        CX "-O0 -g -DDEBUG"
+    )
+endif()
+
+if (CONFIG_MCUX_PRJSEG_config.imx.cortex_m.shared)
+    mcux_add_iar_configuration(
+        AS "-M\"<>\" -w+ -s -j"
+        CC "--diag_suppress=Pa082,Pa050 --endian=little -e --use_c++_inline --silent"
+        CX "--diag_suppress=Pa082,Pa050 --endian=little -e --c++ --silent"
+    )
+
+    mcux_add_mdk_configuration(
+        CC "-std=c99 -fshort-enums -fshort-wchar -ffunction-sections -fno-common -fdata-sections -fno-builtin -mthumb -fno-rtti"
+        CX "-std=c++11 -fshort-enums -fshort-wchar -fno-common -ffunction-sections -fdata-sections -fno-builtin -mthumb -fno-rtti -fno-exceptions"
+        LD "--remove --entry=Reset_Handler --map --xref --callgraph --symbols --info=sizes,totals,unused,veneers --strict --diag_suppress=6314"
+    )
+
+    mcux_add_armgcc_configuration(
+        AS "-mthumb"
+        CC "--specs=nano.specs -Wall -fno-common -ffunction-sections -fdata-sections -fno-builtin -mthumb -mapcs -std=gnu99"
+        CX "--specs=nano.specs -Wall -fno-common -ffunction-sections -fdata-sections -fno-builtin -mthumb -mapcs -fno-rtti -fno-exceptions"
+        LD "--specs=nosys.specs -Wall -fno-common -ffunction-sections -fdata-sections -fno-builtin -mthumb -mapcs -Wl,--gc-sections -Wl,-static -Wl,--print-memory-usage"
+        LIB "-lm -lc -lgcc -lnosys"
+    )
+
+    mcux_add_mcux_configuration(
+        CC "-c -fno-common -ffunction-sections -fdata-sections -fno-builtin -std=gnu99"
+        CX "-c -fno-common -ffunction-sections -fdata-sections -fno-builtin -fno-rtti -std=gnu++11 -fno-exceptions"
+        LD "--specs=nano.specs -fno-common -ffunction-sections -fdata-sections -fno-builtin -nostdlib -Xlinker -no-warn-rwx-segments"
+    )
+endif()
