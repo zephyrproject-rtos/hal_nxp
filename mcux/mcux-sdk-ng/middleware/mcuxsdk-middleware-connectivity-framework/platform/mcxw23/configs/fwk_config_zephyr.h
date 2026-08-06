48d69d52e9e13e6e83b52f988a9fe0ac18c13bcf
Axel Le Bourhis <axel.lebourhis@nxp.com>
2026-08-06 16:54:21 +0200

    mcuxsdk: middleware: connectivity-framework: add fwk_config_zephyr.h
    Override default framework's configs with a specific config for Zephyr.
    
    Signed-off-by: Axel Le Bourhis <axel.lebourhis@nxp.com>

---
+++ mcux/mcux-sdk-ng/middleware/mcuxsdk-middleware-connectivity-framework/platform/mcxw23/configs/fwk_config_zephyr.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2026 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef FWK_CONFIG_ZEPHYR_H_
+#define FWK_CONFIG_ZEPHYR_H_
+
+/*
+ * Zephyr-specific framework configuration overrides.
+ *
+ * This file is included at the top of fwk_config.h under an #ifdef __ZEPHYR__
+ * guard, so it is compiled only for Zephyr builds. Because every flag in
+ * fwk_config.h is #ifndef-protected, any value defined here takes precedence
+ * over the default MCUXpresso SDK value.
+ *
+ * These overrides used to be maintained on the Zephyr side, in
+ * modules/hal_nxp/mcux/mcux-sdk-ng/middleware/connectivity_framework.cmake.
+ * They are centralized here to avoid dual maintenance between the framework
+ * and the Zephyr integration.
+ */
+
+/* On Zephyr the Timer Manager is not used; Zephyr provides its own timer services */
+#ifndef gPlatformUseTimerManager_d
+#define gPlatformUseTimerManager_d 0
+#endif
+
+#endif /* FWK_CONFIG_ZEPHYR_H_ */
--- mcux/mcux-sdk-ng/middleware/mcuxsdk-middleware-connectivity-framework/platform/rw61x/configs/fwk_config.h
+++ mcux/mcux-sdk-ng/middleware/mcuxsdk-middleware-connectivity-framework/platform/rw61x/configs/fwk_config.h
@@ -9,6 +9,14 @@
 
 #include "fwk_platform_definitions.h"
 
+/* On Zephyr, include the Zephyr-specific configuration overrides first so that
+ * they take precedence over the default values defined below (each flag is
+ * #ifndef-protected). This avoids duplicating the overrides in the Zephyr
+ * repository. */
+#ifdef __ZEPHYR__
+#include "fwk_config_zephyr.h"
+#endif
+
 #ifndef gPlatformUseHwParameter_d
 #define gPlatformUseHwParameter_d 0
 #endif
